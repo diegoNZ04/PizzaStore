e7c7c62e713a16d13a969b45aca77ccd383178b7
Diego Amorim <diegoamorim03152004@gmail.com>
2024-11-14 22:07:42 -0300

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Data;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+
+builder.Services.AddSqlite<PizzaDb>(connectionString);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+
+// String Unica
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// Definir dominios, apenas para fins de teste
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    builder =>
+    {
+        builder.WithOrigins(
+            "http://example.com", "*"
+        );
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatePizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatePizza.Name;
+    pizza.Description = updatePizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.Run();
---
+++ PizzaClient/src/Pizza.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import PizzaList from "./PizzaList";
+
+const term = "Pizza";
+const API_URL = "/pizzas";
+const headers = {
+  "Content-Type": "application/json",
+};
+
+function Pizza() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchPizzaData();
+  }, []);
+
+  const fetchPizzaData = () => {
+    fetch(API_URL)
+      .the((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error));
+  };
+
+  const handleCreate = (item) => {
+    console.log(`add item: ${JSON.stringify(item)}`);
+
+    fetch(API_URL, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ name: item.name, description: item.description }),
+    })
+      .then((response) => response.json())
+      .then((returnedItem) => setData([...data, returnedItem]))
+      .catch((error) => setError(error));
+  };
+
+  const handleUpdate = (item) => {
+    console.log(`update item: ${JSON.stringify(updatedData)}`);
+
+    fetch(`${API_URL}/${updatedItem.id}`, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(updatedItem),
+    })
+      .then(() =>
+        setData(
+          data.map((item) => (item.id === updatedItem.id ? updatedItem : item))
+        )
+      )
+      .catch((error) => setError(error));
+  };
+
+  const handleDelete = (id) => {
+    fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+      headers,
+    })
+      .then(() => setData(data.filter((item) => item.id !== id)))
+      .catch((error) => console.error("Error deleting item:", error));
+  };
+
+  return (
+    <div>
+      <PizzaList
+        name={term}
+        data={data}
+        onCreate={handleCreate}
+        onUpdate={handleUpdate}
+        onDelete={handleDelete}
+      />
+    </div>
+  );
+}
+
+export default Pizza;
+
+/*
+Componente responsável por gerenciar os dados, lidar com suas operações e passsar
+para o componente filho PizzaList. 
+
+Use os ganchos useState e useEffect para criar o estado dos dados.
+- useState é usado para gerenciar o data e o maxId;
+- useEffect é usado para definir os dados na solicitação de página inicial.
+*/
